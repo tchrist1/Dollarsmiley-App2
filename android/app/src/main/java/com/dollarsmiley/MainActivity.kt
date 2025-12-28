8184bdba5895f86c9f9779b60ffec9796d375ba9
tchrist1 <54152346+tchrist1@users.noreply.github.com>
2025-12-27 20:28:42 -0500

    Updated settings.gradle

--- android/app/src/main/java/com/dollarsmiley/MainActivity.kt
+++ android/app/src/main/java/com/dollarsmiley/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.helloworld
+package com.dollarsmiley
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/dollarsmiley/MainApplication.kt
+++ android/app/src/main/java/com/dollarsmiley/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.helloworld
+package com.dollarsmiley
 
 import android.app.Application
 import android.content.res.Configuration
